3969b7c2473e5a00ba60634ba5631e2a71369b50
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-02-27 22:57:52 +0100

    update

--- components/multi-step-form/stepForms/PersonalInfoForm.tsx
+++ components/multi-step-form/stepForms/PersonalInfoForm.tsx
@@ -1,10 +1,26 @@
+"use client";
 
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
 
 type Props = {}
 
 export default function PersonalInfoForm({}: Props) {
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { register, reset, watch, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {}
+  });
+
+
+  async function processData(data: any) {
+    console.log(data)
+  }
+
   return (
-    <div>PersonalInfoForm</div>
+    <form onSubmit={handleSubmit(processData)} className='px-12 py-4'>
+      <div className="mb-8"></div>
+    </form>
   )
 }
