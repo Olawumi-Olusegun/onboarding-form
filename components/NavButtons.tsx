7c82eaea569b68ba463d753fe07ffee37451a879
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-03-05 23:19:40 +0100

    Updated..

---
+++ components/NavButtons.tsx
@@ -0,0 +1,12 @@
+
+import { ChevronLast, ChevronLeft, ChevronRight } from "lucide-react";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function NavButtons() {
+    const dispatch = useDispatch();
+    
+  return (
+    <div>NavButtons</div>
+  )
+}
