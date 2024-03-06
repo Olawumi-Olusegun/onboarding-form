0e08f541fd308c4ea5e53667416e12793bec06e3
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-03-06 22:38:45 +0100

    Updated..

--- components/NavButtons.tsx
+++ components/NavButtons.tsx
@@ -7,6 +7,14 @@ export default function NavButtons() {
     const dispatch = useDispatch();
     
   return (
-    <div>NavButtons</div>
+    <div className="flex justify-between items-center">
+      <button
+          type="button"
+          className="inline-flex items-center px-5 py-2 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-slate-900 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-slate-800 dark:bg-blue-600 dark:hover:bg-blue-700"
+        >
+          <ChevronLeft className="w-5 h-5 mr-2" />
+          <span>Previous</span>
+        </button>
+    </div>
   )
 }
