443f99a959c98d80c8327864f3f1809deb05026f
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-03-07 22:00:05 +0100

    Updated..

--- components/NavButtons.tsx
+++ components/NavButtons.tsx
@@ -1,9 +1,10 @@
 
-import { ChevronLast, ChevronLeft, ChevronRight } from "lucide-react";
 import React from "react";
+import { ChevronLast, ChevronLeft, ChevronRight } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function NavButtons() {
+
     const dispatch = useDispatch();
     
   return (
@@ -15,6 +16,14 @@ export default function NavButtons() {
           <ChevronLeft className="w-5 h-5 mr-2" />
           <span>Previous</span>
         </button>
+
+        <button
+        type="submit"
+        className="inline-flex items-center px-5 py-2 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-slate-900 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-slate-800 dark:bg-blue-600 dark:hover:bg-blue-700"
+      >
+        <span>Next</span>
+        <ChevronRight className="w-5 h-5 ml-2" />
+      </button>
     </div>
   )
 }
